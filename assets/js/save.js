9ccb7a4fde1a7bc12f28d9c92e7c57528ebd0dbb
FrancescoDiVitale <francesco.di.vitale.info@gmail.com>
2021-03-28 18:28:38 +0200

    save 20210328_1828

--- assets/js/save.js
+++ assets/js/save.js
@@ -7,11 +7,12 @@ function loadGame(){
 }
 
 function saveInLocal(treeVariables){
-    localStorage.setItem('little_seed_save', JSON.stringify(treeVariables))
+    window.localStorage.setItem('little_seed_save', JSON.stringify(treeVariables))
 }
 
 function loadFromLocal(){
     if(localStorage.getItem('little_seed_save')){
-        treeVariables = JSON.parse(localStorage.getItem('little_seed_save'));
+        treeVariables = JSON.parse(localStorage.getItem("little_seed_save"));
+        console.log(window.localStorage);
     } else { console.log('NOP') }
 }
